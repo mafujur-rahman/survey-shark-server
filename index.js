0e8cef7db617e70743648bcb740799a1222a37ea
mahfuj926 <mahfujrahman.jewel@gmail.com>
2024-06-18 12:26:14 +0600

    complete all code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -9,7 +10,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin:["http://localhost:5173","https://survey-shark-ccd5f.web.app","https://survey-shark-ccd5f.firebaseapp.com" ]
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjwopdy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db('surveyDB').collection('users');
     const surveyCollection = client.db('surveyDB').collection('survey');
@@ -37,11 +40,87 @@ async function run() {
     const reportCollection = client.db('surveyDB').collection('reports');
     const commentCollection = client.db('surveyDB').collection('comments');
 
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log('Received user for JWT:', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      console.log('Generated token:', token);
+      res.send({ token });
+    });
+    
+    // midleware
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+
+    }
+
+
     // Authentication routes
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
-    })
+    });
+
+    // isAdmin api
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    });
+
+    // isSurveyor api
+    app.get('/users/surveyor/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let surveyor = false;
+      if (user) {
+        surveyor = user?.role === 'surveyor';
+      }
+      res.send({ surveyor });
+    });
+
+    // is pro-user api
+    app.get('/users/pro-user/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let proUser = false;
+      if (user) {
+        proUser = user?.role === 'pro-user';
+      }
+      res.send({ proUser });
+    });
+
     app.get('/users', async (req, res) => {
       try {
         const { role } = req.query;
@@ -142,13 +221,13 @@ async function run() {
     });
 
     // get feedbacks
-    app.get('/surveyFeedbacks', async(req,res) =>{
+    app.get('/surveyFeedbacks', async (req, res) => {
       const result = await surveyFeedbacks.find().toArray();
       res.send(result);
     });
 
     // post feedbacks
-    app.post('/surveyFeedbacks', async(req,res) =>{
+    app.post('/surveyFeedbacks', async (req, res) => {
       const newFeedback = req.body;
       const result = await surveyFeedbacks.insertOne(newFeedback);
       res.send(result);
@@ -175,16 +254,16 @@ async function run() {
 
     // surveyor view surveys
     app.get('/responses', async (req, res) => {
-          const surveys = await surveyResponses.find().toArray(); 
-          res.send(surveys);
-  });
-  
-  app.get('/responses/:id', async (req, res) => {
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id) };
-    const result = await surveyResponses.findOne(query);
-    res.send(result);
-});
+      const surveys = await surveyResponses.find().toArray();
+      res.send(surveys);
+    });
+
+    app.get('/responses/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await surveyResponses.findOne(query);
+      res.send(result);
+    });
 
 
     app.get('/surveys/latest', async (req, res) => {
@@ -206,22 +285,22 @@ async function run() {
         const currentDate = new Date();
         const result = await surveyCollection.find({ deadline: { $gte: currentDate.toISOString().split('T')[0] } }).toArray();
         res.json(result);
-    } catch (error) {
+      } catch (error) {
         console.error('Error fetching surveys:', error);
         res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+      }
+    });
 
 
 
-// report related api
+    // report related api
 
-    app.get('/reports', async(req,res) =>{
-      const result =await reportCollection.find().toArray();
+    app.get('/reports', async (req, res) => {
+      const result = await reportCollection.find().toArray();
       res.send(result);
     });
 
-    app.post('/reports', async(req,res) =>{
+    app.post('/reports', async (req, res) => {
       const newReport = req.body;
       const result = await reportCollection.insertOne(newReport);
       res.send(result);
@@ -253,21 +332,27 @@ async function run() {
     });
 
     // comments api
-    app.post('/comments', async(req,res) =>{
+
+    app.get('/comments', async (req, res) => {
+      const result = await commentCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/comments', async (req, res) => {
       const newComment = req.body;
       const result = await commentCollection.insertOne(newComment);
       res.send(result);
     });
 
     // payment get
-    app.get('/payments', async(req,res) =>{
+    app.get('/payments', async (req, res) => {
       const result = await paymentCollection.find().toArray();
       res.send(result);
     });
 
 
     // payment post 
-    app.post('/payments', async(req,res) =>{
+    app.post('/payments', async (req, res) => {
       const payment = req.body;
       const result = await paymentCollection.insertOne(payment);
       res.send(result);
@@ -295,34 +380,34 @@ async function run() {
     app.patch('/users/:email', async (req, res) => {
       const newRole = req.body;
       const email = req.params.email;
-      const user = await userCollection.findOne({ email: email }); 
+      const user = await userCollection.findOne({ email: email });
       if (!user) {
-          return res.status(404).json({ error: 'User not found' });
-      }
-  
-      const UserRole = {
-          $set: {
-              role: newRole.role,
-          }
-      }
-      const result = await userCollection.updateOne({ email: email }, UserRole); 
-      res.send(result);
-  });
-  app.get('/users/:email', async (req, res) => {
-    const email = req.params.email;
-
-    const user = await userCollection.findOne({ email: email });
-    if (!user) {
         return res.status(404).json({ error: 'User not found' });
-    }
+      }
 
-    res.send(user);
-});
+      const UserRole = {
+        $set: {
+          role: newRole.role,
+        }
+      }
+      const result = await userCollection.updateOne({ email: email }, UserRole);
+      res.send(result);
+    });
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+
+      const user = await userCollection.findOne({ email: email });
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      res.send(user);
+    });
 
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
-    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+    // await client.db('admin').command({ ping: 1 });
+    // console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
